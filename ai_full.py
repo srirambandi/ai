Fixed a major bug in maxpool2d function, which didn't store max lacations properly and assigned to max locations to all-zeros case. Now the gradient of maxpool is correct and updated test.py

ai_full.py
@@ -260,10 +260,10 @@ class ComputationalGraph:
 
         out = np.zeros(output_maps_shape)  # output feature maps
 
-        pad_input = np.zeros(pad_shape)
+        pad_x = np.zeros(pad_shape)
         # padded input - copying the actual input onto pad input centre
-        pad_input[:, p[0]:pad_input.shape[1]-p[0], p[1]:pad_input.shape[2]-p[1], :] += x.w
-        pad_input = pad_input.reshape(1, *pad_shape)
+        pad_x[:, p[0]:pad_x.shape[1]-p[0], p[1]:pad_x.shape[2]-p[1], :] += x.w
+        pad_x = pad_x.reshape(1, *pad_shape)
 
         # convolution operation is commutative because of this flipping
         # this step and consequently flipping in backward op are not essential for network
@@ -274,7 +274,7 @@ class ComputationalGraph:
 
                 # multiplying traversed grid portions of padded input feature maps with kernel grids element-wise
                 # and summing the resulting matrix to produce elements of output maps, over all filters and batches
-                out[:, r, c, :] += np.sum(np.multiply(pad_input[:, :, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :], flipped_K.reshape(*K.shape, 1)), axis=(1, 2, 3))
+                out[:, r, c, :] += np.sum(np.multiply(pad_x[:, :, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :], flipped_K.reshape(*K.shape, 1)), axis=(1, 2, 3))
 
         output_feature_maps = Parameter(out.shape, init_zeros=True)
         output_feature_maps.w = out    # any set of output feature map from the batch, has same numeber of maps as filters in the kernel set
@@ -293,13 +293,13 @@ class ComputationalGraph:
 
                             _ = output_feature_maps.dw[:, r, c, :].reshape(fi, 1, 1, 1, batch)
                             # updating the kernel filter set gradient - there will be RxC such updates
-                            flippe_K_grad += np.sum(np.multiply(_, pad_input[:, :, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :]), axis = -1)
+                            flippe_K_grad += np.sum(np.multiply(_, pad_x[:, :, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :]), axis = -1)
 
                     K.dw += np.flip(np.flip(flippe_K_grad), axis=(0, 1))
 
                 if x.eval_grad:
 
-                    pad_input_grad = np.zeros(pad_shape)
+                    pad_x_grad = np.zeros(pad_shape)
 
                     for r in range(output_feature_maps.shape[1]):
                         for c in range(output_feature_maps.shape[2]):
@@ -308,11 +308,11 @@ class ComputationalGraph:
                             # in every batch; similar to kernel gradient method, but the matrix collapses along filters dimention using sum
 
                             _ = output_feature_maps.dw[:, r, c, :].reshape(fi, 1, 1, 1, batch)
-                            pad_input_grad[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :] += np.sum(np.multiply(_, flipped_K.reshape(*K.shape, 1)), axis=0)
+                            pad_x_grad[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :] += np.sum(np.multiply(_, flipped_K.reshape(*K.shape, 1)), axis=0)
 
                     # cutting the padded portion from the input-feature-map's gradient
                     # and updating the gradient of actual input feature map(non-padded) - unpadding and updating
-                    x.dw += pad_input_grad[:, p[0]:pad_input_grad.shape[1]-p[0], p[1]:pad_input_grad.shape[2]-p[1], :]
+                    x.dw += pad_x_grad[:, p[0]:pad_x_grad.shape[1]-p[0], p[1]:pad_x_grad.shape[2]-p[1], :]
 
                 # return (K.dw, x.dw)
 
@@ -338,23 +338,29 @@ class ComputationalGraph:
 
         out = np.zeros((pool_shape))
 
-        pad_input = np.zeros(pad_shape)
-        pad_input[:, p[0]:pad_input.shape[1]-p[0], p[1]:pad_input.shape[2]-p[1], :] += x.w # copying the input onto padded matrix
+        pad_x = np.zeros(pad_shape)
+        pad_x[:, p[0]:pad_x.shape[1]-p[0], p[1]:pad_x.shape[2]-p[1], :] += x.w # copying the input onto padded matrix
 
         for r in range(out.shape[1]):       # convolving operation here(kinda)
             for c in range(out.shape[2]):   # traversing rous and columns of feature map
 
                 # Selecting max element in the current position where kernel sits on feature map
                 # The kernel moves in a convolution manner similar to conv2d
-                _ = pad_input[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :]
+                _ = pad_x[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :]
                 out[:, r, c, :] = np.max(_, axis=(1, 2))
 
-                if self.grad_mode:
+                if self.grad_mode:      # seems inefficient; will improve this whole maxpool op later
 
                     # Also storing value 1 at locations in the input that caused the output values(max locations);
                     # this makes life easy during backprop
+                    # if multiple 0s occur and max is 0 then it shouldn't count. weeding out such cases by assigning
+                    # NaN and later zeroing out their gradient locations too; this was a bug which is fixed now :)
+                    out[:, r, c, :][out[:, r, c, :] == 0] = np.nan
                     _ -= out[:, r, c, :].reshape(fi, 1, 1, batch)
-                    _ = np.where(_ < 0, 0, 1.0)
+                    _[np.isnan(_)] = -1     # removing all zeros locations
+                    # can't use '_' for the below assignment, so using the entire notation :(
+                    pad_x[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :] = np.where(pad_x[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :] < 0, 0, 1.0)
+                    out[:, r, c, :][np.isnan(out[:, r, c, :])] = 0
 
         pool_maps = Parameter(out.shape, init_zeros=True)
         pool_maps.w = out
@@ -369,11 +375,11 @@ class ComputationalGraph:
 
                             # multiplying each 'mask' like volume(single 1 in the volume along all batches) with the gradient
                             # at region whose value was by the mask region's input caused
-                            pad_input[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :] *= pool_maps.dw[:, r, c, :].reshape(fi, 1, 1, batch)
+                            pad_x[:, r*s[0]:r*s[0] + k[0], c*s[1]:c*s[1] + k[1], :] *= pool_maps.dw[:, r, c, :].reshape(fi, 1, 1, batch)
 
                     # cutting the padded portion from the input gradient
                     # and updating the gradient of actual input(non-padded) - unpadding and updating
-                    x.dw += pad_input[:, p[0]:pad_input.shape[1]-p[0], p[1]:pad_input.shape[2]-p[1], :]
+                    x.dw += pad_x[:, p[0]:pad_x.shape[1]-p[0], p[1]:pad_x.shape[2]-p[1], :]
 
                 # return (x.dw)
 

test.py
@@ -2,7 +2,7 @@ import ai_full as ai
 import numpy as np
 
 # I/O preparation
-X = np.stack([np.zeros((3, 6, 6,)), np.random.randint(0, 6, (3, 6, 6))], axis = -1)
+X = np.stack([np.zeros((3, 6, 6)), np.random.randint(-5, 6, (3, 6, 6))], axis = -1)
 
 class test(ai.Model):
     def __init__(self, ):
@@ -38,20 +38,20 @@ while it < 1:
     L.backward()
 
     # testing the library here....
-    print(X.reshape(3, 2, 6, 6))
-    print('Conv2d kernel\n', model.conv1.K.w, '\n')
+    print([X[...,i] for i in range(X.shape[-1])])
+    print('Conv2d kernel\n', [model.conv1.K.w[i,...] for i in range(model.conv1.K.w.shape[0])],'\n')
     print('Conv2d bias\n', model.conv1.b.w, '\n')
-    print('Conv2d ouput\n', res[0].w, '\n gradient\n', res[0].dw, '\n\n')
+    print('Conv2d ouput\n', [res[0].w[...,i] for i in range(res[0].w.shape[-1])], '\n gradient\n', [res[0].dw[...,i] for i in range(res[0].dw.shape[-1])], '\n\n')
 
-    print('ReLU output\n', res[1].w, '\n gradient\n', res[1].dw, '\n\n')
+    print('ReLU output\n', [res[1].w[...,i] for i in range(res[1].w.shape[-1])], '\n gradient\n', [res[1].dw[...,i] for i in range(res[1].dw.shape[-1])], '\n\n')
 
-    print('MaxPool2d output\n', res[2].w, '\n gradient\n', res[2].dw, '\n\n')
+    print('MaxPool2d output\n', [res[2].w[...,i] for i in range(res[2].w.shape[-1])], '\n gradient\n', [res[2].dw[...,i] for i in range(res[2].dw.shape[-1])], '\n\n')
 
-    print('Dropout output\n', res[3].w, '\n gradient\n', res[3].dw, '\n\n')
+    print('Dropout output\n', [res[3].w[...,i] for i in range(res[3].w.shape[-1])], '\n gradient\n', [res[3].dw[...,i] for i in range(res[3].dw.shape[-1])], '\n\n')
 
     # print('Linear weight\n', model. fc1.W.w, '\n')
     # print('Linear bias\n', model.fc1.b.w, '\n')
-    print('Linear layer output\n', res[4].w, '\n gradient\n', res[4].dw, '\n\n')
+    print('Linear layer output\n', [res[4].w[...,i] for i in range(res[4].w.shape[-1])], '\n gradient\n', [res[4].dw[...,i] for i in range(res[4].dw.shape[-1])], '\n\n')
 
     # print('Softmax output', res[5].w, res[5].dw, '\n\n')
 