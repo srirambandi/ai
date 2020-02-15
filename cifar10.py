fixed bug! A silent bug which is corrupting the batched input data is fixed! mnist cifar10 are able to overfit on small mini-batches now

cifar10.py
@@ -57,7 +57,8 @@ def evaluate():
 
     for batch in range(int(len(outputs) / m)):
 
-        input = inputs[batch * test_m : (batch + 1) * test_m].reshape(3, 32, 32, test_m) / 255
+        input = inputs[batch * test_m : (batch + 1) * test_m].reshape(test_m, 3, 32, 32) / 255
+        input =  np.stack([_ for _ in input], axis = -1)
         output = np.array(outputs[batch * test_m : (batch + 1) * test_m])
 
         scores = model.forward(input)
@@ -87,7 +88,8 @@ while loss > 0.1:
 
         for batch in range(int(len(outputs) / m)):
 
-            input = inputs[batch * m : (batch + 1) * m].reshape(3, 32, 32, m) / 255
+            input = inputs[batch * m : (batch + 1) * m].reshape(m, 3, 32, 32) / 255
+            input =  np.stack([_ for _ in input], axis = -1)
             output = outputs[batch * m : (batch + 1) * m]
             onehot = np.zeros((10, m))
             for _ in range(m):

mnist.py
@@ -61,7 +61,8 @@ def evaluate():
 
     for batch in range(int(len(outputs) / m)):
 
-        input = inputs[batch * test_m : (batch + 1) * test_m].reshape(1, 28, 28, test_m) / 255
+        input = inputs[batch * test_m : (batch + 1) * test_m].reshape(test_m, 1, 28, 28) / 255
+        input =  np.stack([_ for _ in input], axis = -1)
         output = np.array(outputs[batch * test_m : (batch + 1) * test_m])
 
         scores = model.forward(input)
@@ -83,7 +84,8 @@ while loss > 0.1:
     for batch in range(int(len(outputs) / m)):
     # for batch in range(1):
 
-        input = inputs[batch * m : (batch + 1) * m].reshape(1, 28, 28, m) / 255
+        input = inputs[batch * m : (batch + 1) * m].reshape(m, 1, 28, 28) / 255
+        input =  np.stack([_ for _ in input], axis = -1)
         output = outputs[batch * m : (batch + 1) * m]
         onehot = np.zeros((10, m))
         for _ in range(m):

test.py
@@ -2,7 +2,7 @@ import ai_full as ai
 import numpy as np
 
 # I/O preparation
-X = np.stack([np.zeros((3, 6, 6)), np.random.randint(-5, 6, (3, 6, 6))], axis = -1)
+X = np.stack([np.zeros((3, 6, 6)), np.random.randint(-5, 6, (3, 6, 6))])
 
 class test(ai.Model):
     def __init__(self, ):
@@ -30,6 +30,7 @@ loss = np.inf
 m = 8
 while it < 1:
 
+    X = np.stack([_ for _ in X], axis = -1)
     res = model.forward(X)
 
     loss = L.loss(res[-1], None).w[0][0]