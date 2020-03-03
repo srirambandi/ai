eb812cca187b37f3bd7c3a6f077880a0494788fa
Sri Ram Bandi <srirambandi.654@gmail.com>
2020-03-03 10:48:17 +0530

    init Wasserstein GAN! Thank you Lilian <3

---
+++ wgan.py
@@ -0,0 +1,17 @@
+import ai_full as ai
+import numpy as np
+
+
+class Generator(ai.Model):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Descriminator(ai.Model):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
