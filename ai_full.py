f74e0960968bbb553c04354c8239fb656167adeb
Sri Ram Bandi <srirambandi.654@gmail.com>
2020-06-02 17:23:10 +0530

    Parameter object now supports basic arithmatic operations - (+, -, *, @, /, **)

--- ai_full.py
+++ ai_full.py
@@ -74,7 +74,7 @@ class Parameter:
         self.grad = np.zeros(self.shape)
 
     def __str__(self):
-        parameter_schema = 'Parameter(shape={}, eval_grad={}) containing:'.format(self.shape, self.eval_grad)
+        parameter_schema = 'Parameter(shape={}, eval_grad={}) containing:\n'.format(self.shape, self.eval_grad)
         parameter_schema += 'Data: {}'.format(self.data)
 
         return parameter_schema
@@ -93,6 +93,52 @@ class Parameter:
 
         for backprop_op in reversed(self.graph.backprop[stop:int(self.node) + 1]):
             backprop_op()
+
+    def __add__(self, other):
+
+        if not isinstance(other, Parameter):
+            other = Parameter(data=other, eval_grad=False, graph=self.graph)
+
+        assert self.shape == other.shape, ('Objects not of same shape. Use G.add() with axis argument', self.shape, other.shape)
+
+        return self.graph.add(self, other)
+
+    def __sub__(self, other):
+
+        if not isinstance(other, Parameter):
+            other = Parameter(data=other, eval_grad=False, graph=self.graph)
+
+        assert self.shape == other.shape, ('Objects not of same shape. Use G.subtract() with axis argument', self.shape, other.shape)
+
+        return self.graph.subtract(self, other)
+
+    def __mul__(self, other):
+
+        if not isinstance(other, Parameter):
+            other = Parameter(data=other, eval_grad=False, graph=self.graph)
+
+        assert self.shape == other.shape, ('Objects not of same shape. Use G.multiply() with axis argument', self.shape, other.shape)
+
+        return self.graph.multiply(self, other)
+
+    def __matmul__(self, other):
+
+        if not isinstance(other, Parameter):
+            other = Parameter(data=other, eval_grad=False, graph=self.graph)
+
+        return self.graph.dot(self, other)
+
+    def __truediv_(self, other):
+
+        if not isinstance(other, Parameter):
+            other = Parameter(data=other, eval_grad=False, graph=self.graph)
+
+        assert self.shape == other.shape, 'Objects not of same shape. Use G.divide() with axis argument'
+
+        return self.graph.divide(self, other)
+
+    def __pow_(self, other, modulo):
+        return self.graph.power(self, other)
 
     # transpose
     def T(self):
