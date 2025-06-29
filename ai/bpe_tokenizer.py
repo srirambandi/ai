cd27aae16eade420cc0b73b54b84302bef59c46d
Sri Ram Bandi <srirambandi.654@gmail.com>
2025-06-29 17:38:34 -0400

    byte pair encoding done

---
+++ ai/bpe_tokenizer.py
@@ -0,0 +1,72 @@
+"""
+Byte Pair Encoding tokenizer based on Andrej Karpathy's Tokenizer video.
+https://www.youtube.com/watch?v=zduSFxRajkE
+"""
+
+
+from typing import List, Tuple
+
+
+def get_pairs(tokens):
+    count = {}
+    for pair in zip(tokens, tokens[1:]):
+        count[pair] = count.get(pair, 0) + 1
+
+    return count
+
+
+def merge_pair(tokens: List[int], pair: Tuple[int, int], new_token_id: int):
+    new_tokens = []
+    i = 0
+    token_1, token_2 = pair
+    while i < len(tokens):
+        if i < len(tokens) - 1 and tokens[i] == token_1 and tokens[i + 1] == token_2:
+            new_tokens.append(new_token_id)
+            i += 2
+        else:
+            new_tokens.append(tokens[i])
+            i += 1
+    
+    return new_tokens
+
+
+class BPETokenizer:
+    def __init__(self):
+        self.vocab = {idx: bytes([idx]) for idx in range(256)}
+        self.merges = {}
+
+    def train(self, corpus: str, num_of_merges: int = 0, SPECIAL_TOKENS: List[str] = []):
+        byte_stream = corpus.encode('utf-8')
+        tokens = list(byte_stream)
+        for i in range(num_of_merges):
+            pairs = get_pairs(tokens)
+            top_pair = max(pairs, key=pairs.get)
+            new_token_id = 256 + i
+            tokens = merge_pair(tokens, top_pair, new_token_id)
+            self.merges[top_pair] = new_token_id
+
+        for (token_1, token_2), token_id in self.merges.items():
+            self.vocab[token_id] = self.vocab[token_1] + self.vocab[token_2]
+
+        for i in range(len(SPECIAL_TOKENS)):
+            it = 256 + num_of_merges + i
+            self.vocab[it] = SPECIAL_TOKENS[i]
+
+    def encode(self, text: str) -> List[int]:
+        byte_stream = text.encode('utf-8')
+        tokens = list(byte_stream)
+
+        while len(tokens) >= 2:
+            pairs = get_pairs(tokens)
+            mergeable_pair = min(pairs, key=lambda pair: self.merges.get(pair, float('inf')))
+            if mergeable_pair not in self.merges:
+                break
+            token_id = self.merges[mergeable_pair]
+            tokens = merge_pair(tokens, mergeable_pair, token_id)
+        
+        return tokens
+
+    def decode(self, tokens: List[int]) -> str:
+        tokens = b"".join(self.vocab[token] for token in tokens)
+        text = tokens.decode("utf-8", errors="replace")
+        return text
