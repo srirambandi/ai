def7b70b24724d3b6c9360c4b328ee775692c493
Sri Ram Bandi <srirambandi.654@gmail.com>
2025-07-01 15:57:17 -0400

    transformer - multi head attn done :)

--- transformer.py
+++ transformer.py
@@ -21,19 +21,18 @@ class SelfAttention(Module):
     this is a causal self attention, because of the type of masking in forward():
     every token only attends to the tokens before it.
     """
-    def __init__(self, d_model, bias=True, graph=G):
+    def __init__(self, d_model, bias=True):
         super(SelfAttention, self).__init__()
         self.Q_proj = Linear(d_model, d_model, bias=bias)
         self.K_proj = Linear(d_model, d_model, bias=bias)
         self.V_proj = Linear(d_model, d_model, bias=bias)
         self.A_proj = Linear(d_model, d_model, bias=bias)
-        self.graph = graph
 
     def forward(self, x):
         Q = self.Q_proj(x)
         K = self.K_proj(x)
         V = self.V_proj(x)
-        d_k = math.sqrt(K.shape[-1])
+        d_k = math.sqrt(K.shape[-1])    # which is d_model generally
         dot_product = Q @ K.transpose(axis0=1, axis1=2)
         scaled_dot_product = dot_product / d_k
         # TODO: masking step here.
@@ -45,9 +44,21 @@ class SelfAttention(Module):
 
 
 class MultiHeadAttention(Module):
-    def __init__(self):
+    def __init__(self, num_heads, d_model, bias=True):
         super(MultiHeadAttention, self).__init__()
-        pass
+        self.num_heads = num_heads
+        for i in range(num_heads):
+            setattr(self, f"attn_{i}", SelfAttention(d_model, bias=bias))
+        
+    def forward(self, x):
+        xs = self.graph.split(x, sections=self.num_heads, axis=-1)
+        ys = []
+        for i in range(self.num_heads):
+            a = getattr(self, f"attn_{i}")(xs[i])
+            ys.append(a)
+        output = self.graph.cat(ys, axis=-1)
+
+        return output
 
 
 class FeedForwardNetwork(Module):
